0f4677d6e617347bc54b256b626bda0181ea526d
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-09-22 23:38:21 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/devpedrogo/aimoviesearcher/AimoviesearcherApplication.java
@@ -0,0 +1,13 @@
+package com.devpedrogo.aimoviesearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimoviesearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimoviesearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpedrogo/aimoviesearcher/AimoviesearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpedrogo.aimoviesearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimoviesearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
